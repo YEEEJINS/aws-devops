e3f1bfcb25389526282be2ba7747f5aa795891a6
CJ <CJ@DESKTOP-QE9FGB2>
2022-11-11 14:20:10 +0900

    first commit

---
+++ src/main/java/net/cj/edu/awsdevops/AwsDevopsApplication.java
@@ -0,0 +1,13 @@
+package net.cj.edu.awsdevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDevopsApplication.class, args);
+	}
+
+}
